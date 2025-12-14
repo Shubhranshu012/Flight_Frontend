828c6a1e1883a146f767e058d50084e05ed3b4f6
Shubhranshu012 <shubhranshu.satpathy@gmail.com>
2025-12-14 14:47:58 +0530

    Search

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { Register } from './components/register/register';
 import { Login } from './components/login/login';
+import { Search } from './components/search/search';
 
 export const routes: Routes = [{path: 'register',component: Register},
-    {path: 'login',component: Login}
+    {path: 'login',component: Login},{path:'search',component: Search}
 ];
