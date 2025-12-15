e1e5eb2b3563ee4d2d45873b4f80d2ff5c0d3fe2
Shubhranshu012 <shubhranshu.satpathy@gmail.com>
2025-12-15 22:53:23 +0530

    Adding NavBar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ReactiveFormsModule],
+  imports: [RouterOutlet, ReactiveFormsModule,Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
