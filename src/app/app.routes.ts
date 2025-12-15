700f4f1832bfd59a719eb486d967802bbb9487f6
Shubhranshu012 <shubhranshu.satpathy@gmail.com>
2025-12-15 15:37:47 +0530

    Adding HomePage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,8 @@ import { Routes } from '@angular/router';
 import { Register } from './components/register/register';
 import { Login } from './components/login/login';
 import { Search } from './components/search/search';
+import { Home } from './components/home/home';
 
 export const routes: Routes = [{path: 'register',component: Register},
-    {path: 'login',component: Login},{path:'search',component: Search}
+    {path: 'login',component: Login},{path:'search',component: Search},{ path: '', component: Home }
 ];
